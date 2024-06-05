ed0f7f728aca77cdec0a1e9da1ad909c6aad4ed0
yanamikko873 <zyonsoupu@gmail.com>
2024-06-05 20:39:06 +0900

    変更内容

--- Fashion .py
+++ Fashion .py
@@ -49,7 +49,6 @@ def upload_file():
 
     return render_template("index.html",answer="")
 
-
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
     app.run(host ='0.0.0.0',port = port)
